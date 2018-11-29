69b8a5f26dcd7907d19b06a0ea709137ed4308a3
David R Connell <davidconnell12@gmail.com>
2018-11-29 16:08:38 -0600

    Changed path generators to reflect file directory changes.

--- Documents/MATLAB/pathGenerators/appendToDocumentationPath.m
+++ Documents/MATLAB/pathGenerators/appendToDocumentationPath.m
@@ -2,6 +2,6 @@ function fullPath = appendToDocumentationPath(pathToAppend)
 	% Gets the path to the file or directory specified by pathToAppend
 	% within the documention folder (~/Google_Drive/Rush/Documentation).
 
-	baseDir = '~/Google_Drive/Rush/Documentation/';
+	baseDir = '$work/docs/';
 	fullPath = strcat(baseDir, pathToAppend);
 end
--- Documents/MATLAB/pathGenerators/appendToPresentationPath.m
+++ Documents/MATLAB/pathGenerators/appendToPresentationPath.m
@@ -2,6 +2,6 @@ function fullPath = appendToPresentationPath()
 	% Gets the path to the file or directory specified by pathToAppend
 	% within the presentation folder (~/Google_Drive/Rush/Presentations).
 
-	baseDir = '~/Google_Drive/Rush/Presentations/';
+	baseDir = '$work/presentations/';
 	fullPath = strcat(baseDir, pathToAppend);
 end
