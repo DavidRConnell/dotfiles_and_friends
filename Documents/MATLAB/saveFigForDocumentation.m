5042b3e8da917b89a1f832910ea7e6580090c2b6
David R Connell <davidconnell12@gmail.com>
2019-01-27 18:58:31 -0600

    Added usage.

--- Documents/MATLAB/saveFigForDocumentation.m
+++ Documents/MATLAB/saveFigForDocumentation.m
@@ -1,6 +1,8 @@
 function saveFigForDocumentation(name, project)
 	% Wrapper for saveFigForLatex to simplify saving to the documentation
 	% directory.
+	% 
+	% USAGE: saveFigForDocumentation(figureName, project);
 
 	projectPath = appendToDocumentationPath(project);
 	saveFigForLatex(name, projectPath);
