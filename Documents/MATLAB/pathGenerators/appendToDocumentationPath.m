0a9003b6a3ddf26881f2c59e0b56c0e6e88d494b
David R Connell <davidconnell12@gmail.com>
2019-02-07 16:19:07 -0600

    Fixed output name.

--- Documents/MATLAB/pathGenerators/appendToDocumentationPath.m
+++ Documents/MATLAB/pathGenerators/appendToDocumentationPath.m
@@ -2,5 +2,5 @@ function newPath = appendToDocumentationPath(addendum)
 	% Gets the path to the file or directory specified by pathToAppend
 	% within the documention folder (~/Google_Drive/Rush/Documentation).
 	
-	fullPath = appendToWorkPath(strcat('docs/', addendum));
+	newPath = appendToWorkPath(strcat('docs/', addendum));
 end
