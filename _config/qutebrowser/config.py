0c17bbb1acb14b284050a1f38b58b60b649cff59
David R Connell <davidconnell12@gmail.com>
2019-12-07 18:55:27 -0600

    Add: Qutebrowser configs

---
+++ _config/qutebrowser/config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import dracula.draw
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
+
+c.hints.mode = 'number'
+c.fonts.monospace = 'hack nerd font'
+c.url.start_pages = ["https://en.wikipedia.org/wiki/Special:Random"]
+c.url.default_page = "https://en.wikipedia.org/wiki/Special:Random"
+c.editor.command = ["emacsclient", "-c", "{file}"]
+
+config.unbind('<Ctrl-N>')
+config.unbind('<Ctrl-P>')
+config.bind('<Ctrl-N>', 'completion-item-focus --history next', mode='command')
+config.bind('<Ctrl-P>', 'completion-item-focus --history prev', mode='command')
+config.bind('<Ctrl-Space>', 'completion-item-fucus-next', mode='command')
+config.bind('<Ctrl-Space>', 'completion-item-fucus-next', mode='prompt')
+
+config.bind('<Ctrl-D>', 'quit', mode='normal')
+config.bind('<Ctrl-I>', 'forward', mode='normal')
+config.bind('<Ctrl-O>', 'back', mode='normal')
