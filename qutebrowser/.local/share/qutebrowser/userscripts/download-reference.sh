fdef9afb7202b2d62f6e24e2153cd29d52cb76ab
David R Connell <davidconnell12@gmail.com>
2021-01-22 23:25:29 -0600

    Fix: add safety check to ensure URL points to a PDF

--- qutebrowser/.local/share/qutebrowser/userscripts/download-reference.sh
+++ qutebrowser/.local/share/qutebrowser/userscripts/download-reference.sh
@@ -3,5 +3,10 @@
 tmp_name=/tmp/qutebrowser_$(date "+%Y%m%d%H%M%s").pdf
 pdf_url=$(echo "$QUTE_URL" | sed -n "s_.*\(https://.*\.pdf\).*_\1_p")
 
-wget --user-agent="$QUTE_USER_AGENT" "$pdf_url" -O $tmp_name
+if [ -z "$pdf_url" ]; then
+    >&2 echo "No PDF at URL: $QUTE_URL."
+    exit 1
+fi
+
+wget -A pdf --user-agent="$QUTE_USER_AGENT" "$pdf_url" -O $tmp_name
 emacsclient -ca '' $tmp_name
