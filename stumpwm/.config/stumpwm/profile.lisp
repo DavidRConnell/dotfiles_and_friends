6545a2766e3552d2f3a4d96d1f6d3483c2f74613
David R Connell <davidconnell12@gmail.com>
2021-02-28 23:05:59 -0600

    Clean: move stumpwm to .config

--- stumpwm/.config/stumpwm/profile.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :stumpwm)
-
-(defvar *browser* "qutebrowser")
-(defvar *alt-browser* "chromium")
-(defvar *terminal* "alacritty")
-(defvar *editor* "emacsclient -c")
-(defvar *email-url* "webemail.rush.edu")
