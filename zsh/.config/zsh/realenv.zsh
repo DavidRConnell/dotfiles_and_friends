0a7f7df8e4a59022d331a38a30c07ea440da5676
David R Connell <davidconnell12@gmail.com>
2021-10-04 22:50:16 -0500

    Add: zshenv in-spite of nix
    - Want to use home-manager as a plugin manager for zsh but
    home-manager tries to destroy modularity and doesn't write the zsh
    configs the way I want them so use my own zsh files and source them
    with home-manager.

---
+++ zsh/.config/zsh/realenv.zsh
@@ -0,0 +1,17 @@
+# XDG directories
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+
+# ZSH files
+export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+export HISTFILE="$XDG_DATA_HOME/zsh/history"
+export HISTSIZE=10000
+export SAVEHIST=10000
+
+# Editors
+export EDITOR="emacsclient -ca ''"
+export VISUAL="emacsclient -ca ''"
+
+export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+export STOW_DIR="$HOME/.dotfiles"
